49443e571ca24be9ba146ff68e4f22b556a65876
Alexis Brodeur <brodeuralexis@gmail.com>
2016-05-20 10:18:58 -0400

    Add comments

--- src/actions/session.js
+++ src/actions/session.js
@@ -4,8 +4,16 @@ import Backend from '../modules/backend';
 import constants from '../constants';
 import BaseActions from './base';
 
+/**
+ * Classe permettant de modifier l'utilisateur courant de l'application.
+ */
 class SessionActions extends BaseActions {
 
+    /**
+     * Connecte un utilisateur à l'application
+     * 
+     * @param {{ username: string }} user L'utilisateur à connecter
+     */
     connect(user) {
         return Backend.connect(user).then(({ user, users }) => {
             this.dispatch(constants.SESSION.POPULATE, {

1aa1999f5f644460504e5861afba9afbac1343e1
Alexis Brodeur <brodeuralexis@gmail.com>
2016-05-20 10:14:23 -0400

    Add comments

--- src/modules/key-mirror.js
+++ src/modules/key-mirror.js
@@ -1,9 +1,26 @@
 'use strict';
 
+/**
+ * Transforme un segment lors de l'opération de mirroir
+ * d'objet.
+ * 
+ * @param {string} value La valeur à transformer
+ * @return {string} La valeur transformée
+ */
 function defaultTransform(value) {
     return String(value).toLowerCase();
 }
 
+/**
+ * Crée un objet mirroir à partir d'un objet de base.  Le mirroir comporte les mêmes clefs,
+ * mais leurs valeurs est une version transformé du chemin vers leurs clefs respective.
+ * 
+ * @param {object} object L'objet à transformer en mirroir
+ * @param {string} [delimiter=' '] La valeur séparant chaque segments
+ * @param {string} [prefix=' '] Le prefix à rajouter à chaque valeur
+ * @param {function(string):string} [transform=defaultTransform] La transformation à appliquer à chaques segments
+ * @return {object} L'objet mirroir
+ */
 export default function keyMirror(object, delimiter = ' ', prefix = '', transform = defaultTransform) {
     return Object.keys(object).reduce((mirror, key) => {
         if (object[key] && typeof object[key] === 'object') {

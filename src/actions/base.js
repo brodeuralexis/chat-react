3221589723db7a0b7edbecd3bb802d4ef8af1c2e
Alexis Brodeur <brodeuralexis@gmail.com>
2016-05-20 09:50:51 -0400

    Add comments

--- src/actions/base.js
+++ src/actions/base.js
@@ -2,8 +2,18 @@
 
 import Store from '../store';
 
+/**
+ * Classe abstraite représentant une liste d'actions sur une ressource.
+ */
 export default class BaseActions {
 
+    /**
+     * Émets des données sous un certain type d'action.  Il est préférable
+     * que la valeur de "type" provienne du fichier "src/constants/index.js".
+     * 
+     * @param {string} type Le type de l'action
+     * @param {object] data Les données a émettres
+     */
     dispatch(type, data) {
         Store.dispatch({
             ...data,

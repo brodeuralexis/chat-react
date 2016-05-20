2ea99ef7028503531856195702190768eb25bb6f
Alexis Brodeur <brodeuralexis@gmail.com>
2016-05-20 10:20:36 -0400

    Add comments

--- src/actions/typing.js
+++ src/actions/typing.js
@@ -4,12 +4,22 @@ import Backend from '../modules/backend';
 import constants from '../constants';
 import BaseActions from './base';
 
+/**
+ * Classe permettant d'indiquer au serveur si l'utilisateur courant est entrain d'écrire
+ * un message.
+ */
 class TypingActions extends BaseActions {
 
+    /**
+     * Indique au serveur que l'utilisateur est entrain d'écrire un message.
+     */
     start() {
         return Backend.startTyping();
     }
 
+    /**
+     * Indique au serveur que l'utilisateur a arrêté d'écrire un message.
+     */
     stop() {
         return Backend.stopTyping();
     }

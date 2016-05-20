5c3270a84c70623166ca826dcca7603b4b21f107
Alexis Brodeur <brodeuralexis@gmail.com>
2016-05-20 10:17:39 -0400

    Add comments

--- src/actions/message.js
+++ src/actions/message.js
@@ -4,8 +4,16 @@ import Backend from '../modules/backend';
 import constants from '../constants';
 import BaseActions from './base';
 
+/**¸
+ * Classe permettant d'effectuer des actions auprès des messages.
+ */
 class MessageActions extends BaseActions {
 
+    /**
+     * Envois un message au serveur.
+     * 
+     * @param {{ content: string }} message Le message à envoyer au serveur
+     */
     send(message) {
         return Backend.sendMessage(message).then(({ user, message }) => {
             this.dispatch(constants.MESSAGE.APPEND, {
